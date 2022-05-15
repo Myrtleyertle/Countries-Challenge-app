9ae9923ab17c936abca7902983740c25bd6c3121
Myrtleyerlte <dylan.rosevear143@gmail.com>
2022-05-15 11:53:00 -0500

    --

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import Countries from "./routes/Countries";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import useLocalStorage from "use-local-storage";
 import './App.css';
+import Country from "./routes/Country";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
+  const [darkMode, setDarkMode] = useState(false);
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    setDarkMode((prevState) => !prevState);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{height: "100vh"}} data-theme={theme}>
+      <Navbar darkMode={darkMode} switchTheme={switchTheme}/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Countries />} />
+          <Route path="/Countries/:name" element={<Country />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
